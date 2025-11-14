cb027c74a5412f99abe3bd272e87293faf74d996
Adjei Edmond <adjeiedmond582@gmai.com>
2025-11-14 11:48:30 +0000

    made changes to the Entry.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./Components/Header.jsx";
+import Entry from "./Components/Entry.jsx";
 import "./index.css";
 
 export default function App(){
     return(
         <>
             <Header />
+            <Entry />
         </>
     )
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,9 @@
+import globe from '../assets/Globe.png';
+
 export default function Header(){
     return(
         <header className="header">
-            <img className="globe" src="./assets/Globe.png" alt="" />
+            <img className="globe" src={globe} alt="globe" />
             <h1>my travel journal</h1>
         </header>
     )
